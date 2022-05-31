d421a8082bdb925c6adca825bd94874ea1d50f54
Solange <sol.sch1990@gmail.com>
2022-05-31 20:48:30 +0200

    Created Json file and read this for instanting GazePointData

--- Assets/Script/tracker.cs
+++ Assets/Script/tracker.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using Tobii.Gaming;
+using System.IO;
 
 public class tracker : MonoBehaviour
 {
@@ -13,43 +16,120 @@ public class tracker : MonoBehaviour
     }
     
     [SerializeField] List<TimePos> timePos;
+    [SerializeField] GameObject point;
+
+    List<Vector2> positionList = new List<Vector2>();
+    public float timer;
+
+    [Serializable]
+    struct SaveData
+    {
+        public List<TimePos> timePos;
+    }
+
+    bool pointDisplay = true;
+
 
     void Start()
     {
-        
+        Debug.Log(InputSystem.devices);
     }
 
     void Update()
     {
-        // retourne la position et le temps
-        TimePos newTimePos = new TimePos();
-        newTimePos.Positions = InputPosition();
-        newTimePos.time = Time.deltaTime;
-
-        if(timePos.Count >= 1)
-        {
-            // rassembler le temps des positions identiques qui se suivent
-            if(newTimePos.Positions == timePos[timePos.Count - 1].Positions)
-            {
-                newTimePos.time += timePos[timePos.Count - 1].time;
-                timePos[timePos.Count - 1] = newTimePos;
-            }
-            else
-            {
-                // stock les positions des inputs (pour le moment la souris)
-                timePos.Add(newTimePos);
-            }
-        }
-        else
-        {
-            timePos.Add(newTimePos);
-        }
+        pointDisplay = false;
         
+        positionList.Add(InputPosition());
+        timer += Time.deltaTime;
+        if (timer >= 0.2)
+        {
+            // enregistre les position toutes les secondes
+            TimePos newTimePos = new TimePos();
+            newTimePos.Positions = InputPosition();
+            newTimePos.time = Time.time;
+            // fait la moyenne
+            Vector2 sum = Vector2.zero;
+            for (int i = 0; i < positionList.Count; i++)
+            {
+                sum += positionList[i];
+            }
+            sum /= positionList.Count;
+            newTimePos.Positions = sum;
+            timePos.Add(newTimePos);
+        
+            if (pointDisplay = false)
+            {
+                GameObject.Instantiate(point, newTimePos.Positions, Quaternion.identity, transform);
+            }
+            timer = 0;
+            positionList.Clear();
+        }
     }
 
-    // Fonction qui retourne la position des inputs (la souris)
+    //Fonction qui retourne la position des inputs(eyetracker)
     public Vector2 InputPosition()
     {
-        return Input.mousePosition;
+        GazePoint gazePoint = TobiiAPI.GetGazePoint();
+        return Camera.main.ScreenToWorldPoint(gazePoint.Screen);
+    }
+
+    // Fonction qui permet de créer un Json
+    public void CreateJsonGazePoint()
+    {
+        SaveData saveData = new SaveData();
+        saveData.timePos = timePos;
+        string json = JsonUtility.ToJson(saveData, true);
+        //StreamWriter writer = new StreamWriter("GazePoint.txt", false);
+        //writer.Write(json);
+        WriteToFile(json);
+        Debug.Log(json);
+    }
+
+    // Fonction qui permet d'écrire le Json
+    private void WriteToFile(string json)
+    {
+        FileStream fileStream = new FileStream("GazePoint.txt", FileMode.Create);
+
+        using (StreamWriter writer = new StreamWriter(fileStream))
+        {
+            writer.Write(json);
+        }
+    }
+
+    // Fonction qui permet de lire le Json
+    private SaveData ReadJsonGazePoint()
+    {
+        // Lecture du json
+        string json;
+        StreamReader reader = new StreamReader("GazePoint.txt", true);
+        json = reader.ReadToEnd();
+        Debug.Log(json);
+        // Traduit le string en structure SaveData
+        SaveData saveData = new SaveData();
+        saveData = JsonUtility.FromJson<SaveData>(json);
+
+        return saveData;
+    }
+
+    // Fonction qui permet d'afficher le gazepoint du Json
+    public void DrawJsonGazePoint()
+    {
+        // supprime les gazepoint préexistants
+        int childs = transform.childCount;
+        for (int i = 0; i < childs; i++)
+        {
+            GameObject.Destroy(transform.GetChild(i).gameObject);
+        }
+
+        // Lecture des positions
+        SaveData saveData;
+        saveData = ReadJsonGazePoint();
+
+        // Instantiation du gazepoint contenu dans le Json
+        for (int i = 0; i < saveData.timePos.Count; i++)
+        {
+            Vector2 positionGazePoint = saveData.timePos[i].Positions;
+            GameObject.Instantiate(point, positionGazePoint, Quaternion.identity, transform);
+        }
     }
 }
